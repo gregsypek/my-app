b99a3624e6d3c0edb3e7cef1c4a80ce4ae64bac0
Greg <47268138+gregsypek@users.noreply.github.com>
2024-10-28 09:50:33 +0100

    setup layout

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<>
+			<h1 className="text-3xl">Welcome to dashboard</h1>;
+		</>
+	);
+}
---
+++ app/components/hello.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+function Hello() {
+	console.log("🚀 ~ hello ~ return:", "client hello");
+	return <div>hello</div>;
+}
+
+export default Hello;
